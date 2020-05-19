882d574fc1c3ce33febe255623b0a0339f7ea84e
muhammadumarfarooq <develop.umar@gmail.com>
2020-05-19 19:44:30 +0500

    momentjs format

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
 
 function App() {
+  console.log(moment());
+  console.log(moment().format("DD/MM/YYYY HH:mm:ss"));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Moment Js</h1>
+      <h2>{moment().format("DD/MM/YYYY HH:mm:ss")}</h2>
     </div>
   );
 }
