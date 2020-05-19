3c21bba627b8692593cff8a85ed0d2da42a275da
muhammadumarfarooq <develop.umar@gmail.com>
2020-05-19 20:10:31 +0500

    momentjs format different values

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import moment from 'moment';
 function App() {
   console.log(moment());
   console.log(moment().format("DD/MM/YYYY HH:mm:ss"));
+  console.log(moment().format("DDD/MMM/YYYY HH:mm:ss")); //years day 115, 150 etc. month in english
+  console.log(moment().format("ddd/MMM/YYYY HH:mm:ss")); //day in english
+  console.log(moment().format("Do/MMMM/YY")); //add suffix
+  console.log(moment().format("Do--MMMM--YY")); //special format
   return (
     <div className="App">
       <h1>Moment Js</h1>
