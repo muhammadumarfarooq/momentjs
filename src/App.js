221be26545977611981766cd9c6ea6e94bd22ddf
muhammadumarfarooq <develop.umar@gmail.com>
2020-05-19 20:29:18 +0500

    momentjs time format

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 import moment from 'moment';
 
 function App() {
-  console.log(moment());
-  console.log(moment().format("DD/MM/YYYY HH:mm:ss"));
-  console.log(moment().format("DDD/MMM/YYYY HH:mm:ss")); //years day 115, 150 etc. month in english
-  console.log(moment().format("ddd/MMM/YYYY HH:mm:ss")); //day in english
-  console.log(moment().format("Do/MMMM/YY")); //add suffix
-  console.log(moment().format("Do--MMMM--YY")); //special format
+  const a = moment();
+  console.log(a);
+  // DATE
+  console.log(a.format("DD/MM/YYYY HH:mm:ss"));
+  console.log(a.format("DDD/MMM/YYYY HH:mm:ss")); //years day 115, 150 etc. month in english
+  console.log(a.format("ddd/MMM/YYYY HH:mm:ss")); //day in english
+  console.log(a.format("Do/MMMM/YY")); //add suffix
+  console.log(a.format("Do--MMMM--YY")); //special format
+  // TIME
+  console.log(a.format("HH:mm:ss")); // 24 hour time
+  console.log(a.format("hh:mm:ss")); // 12 hour time
+  console.log(a.format("hh:mm A")); // 12 hour time with am/pm
+  
   return (
     <div className="App">
       <h1>Moment Js</h1>
